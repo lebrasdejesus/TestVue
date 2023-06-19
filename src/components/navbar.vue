e7bb19acfca22cfe59aa176a2c5cc171bee031c3
syneido <syneido@MacBook-Pro-de-syneido.local>
2023-06-19 17:45:44 +0200

    debut des exos js sur vue

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,23 +1,44 @@
 <template>
-    <div class="container">
-    </div>
+  <div class="container">
+    <div class="containerImage"></div>
+    <div ><img class="logo" alt="Vue logo" src="../assets/logo.png"></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 
 
 </template>
 
 <script>
 export default {
-  name: 'Nav',
-  props: {
-    msg1: String
-  }
+  name: 'NavB',
+
 }
 </script>
 
 <style>
-    .container{
-        width: 100wv;
-        height: 20vh;
-        background-color: red;
-    }
+.container {
+  width: 100wv;
+  height: 8vh;
+  max-height: 8vh;
+  background-color: white;
+  display: grid;
+  grid-template-columns: 0.5fr 2fr 2fr 2fr 2fr 2fr 2fr 2fr 2fr 0.5fr;
+  border: 2px solid red;
+}
+.container > div{
+  padding: 0px 0px;
+  background-color: #8be5bc;
+  border: 2px solid RGB(120,40,160);
+}
+.logo{
+  max-height: 7vh;
+}
 </style>
+
