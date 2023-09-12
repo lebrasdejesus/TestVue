f13e1f1b305f8d514fbe46aa188944047cd11d46
Fred Chambinaud <f.chambinaud@gmail.com>
2023-09-12 15:59:40 +0200

    ajout du publicpath

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+//ajout de lignes pour pouvoir publier sur git
+//trouvées sur https://dfaveris.medium.com/publier-une-application-vuejs-sur-github-ee7662c94667
+const publicPath = process.env.NODE_ENV === 'production' ? '/TestVue/' : '/'
+module.exports = {
+  publicPath: publicPath,
+}
