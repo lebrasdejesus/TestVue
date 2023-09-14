8385977a2fe81bb70a80d18c86f61cb0de07f1fb
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2023-09-14 16:04:18 +0200

    testdeskapp

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -4,7 +4,7 @@
     voici quelques petites choses faites sur Vue.js en stage : <br />- des
     anciens exercices de formation, refaits en Vue.js (contenu 1 et 3), <br />-
     des petites choses nouvelles (contenu 2 et 4), <br />- et enfin une page un
-    peu plus aboutie en contenu 5
+    peu plus aboutie en contenu 5...
   </p>
 </template>
   
